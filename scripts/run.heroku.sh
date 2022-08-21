3f0a7bc80c72f9b4790c3fd772341f9b3c560298
MANSUOOR <12770700+molafir@users.noreply.github.com>
2022-08-21 12:47:53 +0430

    Update run.heroku.sh

--- scripts/run.heroku.sh
+++ scripts/run.heroku.sh
@@ -1,8 +1,5 @@
 PATH=$(pwd):${PATH}
 
-wget -q https://yt-dl.org/downloads/latest/youtube-dl
-chmod +x youtube-dl
-
 curl -s https://raw.githubusercontent.com/callsmusic/trtmp/main/scripts/install.debian.sh | bash
 chmod +x linux-amd64
 ./linux-amd64
